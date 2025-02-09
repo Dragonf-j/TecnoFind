f2309d7cfa34f7385b5695ba2840a19b4693bba1
Dragonf-j <55980623+Dragonf-j@users.noreply.github.com>
2025-02-09 17:16:18 +0100

    update Form laptops
    Se corrigen errores menores del formulario para elegir portatil, ademas se añaden mas pregutnas

--- laptops/static/js/formLaptop.js
+++ laptops/static/js/formLaptop.js
@@ -6,6 +6,8 @@ $(document).ready(function () {
     const buttonNext = $('#nextStep');
     const buttonSend = $('#send');
     const firstDiv = $('#firtdiv');
+    const buttonReview = $('#reviewButton');
+    const Divreview = $('#review');
     
     // Declaración de variables
     let Step = 0;
@@ -16,38 +18,73 @@ $(document).ready(function () {
         Portabilidad: '',
         Presupuesto: '',
         Almacenamiento:'',
-        VelocidadDisco:'',
+        Velocidad: '',
         Bateria:'',
-        conexiones:'',
+        conexiones: [],
         CantPuertos: '',
-        Extras:'',
-        marca:''
+        Extras: [],
+        marca:'',
+        otraMarca:''
     };
 
-    // Array para guardar las preguntas
+    // Array para guardar las preguntas con un número que indique su posición
     let questions = [
-        { id: 'rendimiento', label: '1.1- ¿Qué potencia necesitas?', options: [{text: 'bajo', value: 'bajo'}, {text: 'Medio', value: 'medio'}, {text: 'alto', value: 'alto'}, {text: 'Top', value: 'top'}] },
-        { id: 'portabilidad', label: '2- ¿Qué tamaño de pantalla prefieres?', options: [{text: 'Pequeño (entre 13 y 14 pulgadas)', value: 'pequeño'}, {text: 'Mediano (entre 14 y 16 pulgadas)', value: 'mediano'}, {text: 'Grande (17 pulgadas o mas)', value: 'grande'}] },
-        { id: 'presupuesto', label: '3- ¿Cuál es tu presupuesto?', options: [{text: 'bajo (Menos de 500€)', value: 'bajo'}, {text: 'medio (Entre 500 y 1000)', value: 'medio'}, {text: 'alto (Entre 1000 y 1500)', value: 'alto'}, {text: 'Muy alto (Mas de 2000)', value: 'muy_alto'}] },
-        { id: 'almacenamiento', label: '4- ¿Qué capacidad de almacenamiento necesitas?', options: [{text: '256 GB', value: '256'}, {text: '512 GB', value: '512'}, {text: '1 TB', value: '1024'}, {text: '2 TB', value: '2048'}] },
-        { id: 'velocidad', label: '4.1 - ¿Qué velocidad de almacenamiento quieres?', options: [{text: 'Hdd (Lento pero con alta capacidad de almacenamiento)', value: 'hdd'}, {text: 'Hibidro (Mezcla entre HDD y SSD)', value: 'hibidro'}, {text: 'Rapido (SSD)', value: 'ssd'}, {text: 'NoImporta', value: 'no_importa'}] },
-        { id: 'bateria', label: '5- ¿Qué duración de batería necesitas?', options: [{text: 'corta (Menos de 4 horas)', value: 'corta'}, {text: 'media (4-8 horas)', value: 'media'}, {text: 'larga (Más de 8 horas)', value: 'larga'}] },
-        { id: 'conexiones', label: '6- ¿Qué tipo de conexiones necesitas?', options: [{text: 'USB', value: 'usb'}, {text: 'HDMI', value: 'hdmi'}, {text: 'Ethernet', value: 'ethernet'}, {text: 'WiFi', value: 'wifi'}] },
-        { id: 'cantPuertos', label: '7- ¿Cuántos puertos necesitas?', options: [{text: '1', value: '1'}, {text: '2', value: '2'}, {text: '3', value: '3'}, {text: '4', value: '4'}] },
-        { id: 'extras', label: '8- ¿Qué extras necesitas?', options: [{text: 'Lector de huellas', value: 'lector'}, {text: 'Cámara web', value: 'camara'}, {text: 'Teclado retroiluminado', value: 'teclado'}, {text: 'Ninguno', value: 'ninguno'}] }
+        { id: 'rendimiento', label: '1.1- ¿Qué potencia necesitas?', position: 1, options: [{text: 'bajo', value: 'bajo'}, {text: 'Medio', value: 'medio'}, {text: 'alto', value: 'alto'}, {text: 'Top', value: 'top'}] },
+        { id: 'portabilidad', label: '2- ¿Qué tamaño de pantalla prefieres?', position: 2, options: [{text: 'Pequeño (entre 13 y 14 pulgadas)', value: 'pequeño'}, {text: 'Mediano (entre 14 y 16 pulgadas)', value: 'mediano'}, {text: 'Grande (17 pulgadas o mas)', value: 'grande'}] },
+        { id: 'presupuesto', label: '3- ¿Cuál es tu presupuesto?', position: 3, options: [{text: 'bajo (Menos de 500€)', value: 'bajo'}, {text: 'medio (Entre 500 y 1000)', value: 'medio'}, {text: 'alto (Entre 1000 y 1500)', value: 'alto'}, {text: 'Muy alto (Mas de 2000)', value: 'muy_alto'}] },
+        { id: 'almacenamiento', label: '4- ¿Qué capacidad de almacenamiento necesitas?', position: 4, options: [{text: '256 GB', value: '256'}, {text: '512 GB', value: '512'}, {text: '1 TB', value: '1024'}, {text: '2 TB', value: '2048'}] },
+        { id: 'velocidad', label: '4.1 - ¿Qué velocidad de almacenamiento quieres?', position: 5, options: [{text: 'Hdd (Lento pero con alta capacidad de almacenamiento)', value: 'hdd'}, {text: 'Hibidro (Mezcla entre HDD y SSD)', value: 'hibidro'}, {text: 'Rapido (SSD)', value: 'ssd'}, {text: 'NoImporta', value: 'no_importa'}] },
+        { id: 'bateria', label: '5- ¿Qué duración de batería necesitas?', position: 6, options: [{text: 'corta (Menos de 4 horas)', value: 'corta'}, {text: 'media (4-8 horas)', value: 'media'}, {text: 'larga (Más de 8 horas)', value: 'larga'}] },
+        { id: 'conexiones', label: '6- ¿Qué tipo de conexiones necesitas?', position: 7, options: [{text: 'USB', value: 'usb'}, {text: 'HDMI', value: 'hdmi'}, {text: 'Ethernet', value: 'ethernet'}, {text: 'WiFi', value: 'wifi'}], type: 'checkbox' },
+        { id: 'cantPuertos', label: '7- ¿Cuántos puertos necesitas?', position: 8, options: [{text: '1', value: '1'}, {text: '2', value: '2'}, {text: '3', value: '3'}, {text: '4'}] },
+        { id: 'extras', label: '8- ¿Qué extras necesitas?', position: 9, options: [{text: 'Lector de huellas', value: 'lector'}, {text: 'Cámara web', value: 'camara'}, {text: 'Teclado retroiluminado', value: 'teclado'}, {text: 'Ninguno', value: 'ninguno'}], type: 'checkbox' },
+        {id: 'marca', label: '9- ¿Tienes Preferencia de marca?', position: 10, options: [{text: 'HP', value: 'hp'}, {text: 'Lenovo', value: 'lenovo'}, {text: 'Asus', value: 'asus'}, {text: 'Acer', value: 'acer'}, {text: 'Dell', value: 'dell'}, {text: 'MSI', value: 'msi'}, {text: 'Apple', value: 'apple'}, {text: 'Otra', value: 'otra'}]},
+        { id: 'otraMarca', label: '9.1- ¿Cuál es la marca de tu preferencia?', position: 11, type: 'text' }
     ];
 
     // Función para generar las preguntas dinámicamente
     function generateQuestion(step) {
         if (step < questions.length) {
-            const question = questions[step];
-            let opciones = `<label for="${question.id}">${question.label}</label><select id="${question.id}" name="${question.id}" class="form-select">`;
-            question.options.forEach(option => {
-                opciones += `<option value="${option.value}">${option.text}</option>`;
-            });
-            opciones += `</select>`;
+            const question = questions.find(q => q.position === step);
+            let opciones = `<label for="${question.id}">${question.label}</label>`;
+            if (question.type === 'checkbox') {
+                question.options.forEach(option => {
+                    opciones += `<div class="form-check">
+                                    <input class="form-check-input" type="checkbox" value="${option.value}" id="${option.value}">
+                                    <label class="form-check-label" for="${option.value}">
+                                        ${option.text}
+                                    </label>
+                                </div>`;
+                });
+            } else if (question.type === 'text') {
+                opciones += `<input type="text" id="${question.id}" name="${question.id}" class="form-control">`;
+            } else {
+                opciones += `<select id="${question.id}" name="${question.id}" class="form-select">`;
+                question.options.forEach(option => {
+                    opciones += `<option value="${option.value}">${option.text}</option>`;
+                });
+                opciones += `</select>`;
+                if (question.id === 'marca') {
+                    opciones += `<div id="otraMarcaDiv" style="display:none;">
+                                    <label for="otraMarcaInput">Especifica la marca:</label>
+                                    <input type="text" id="otraMarcaInput" class="form-control">
+                                </div>`;
+                }
+            }
             dynamicDiv.empty();
             dynamicDiv.append(opciones);
+
+            if (question.id === 'marca') {
+                $('#marca').change(function() {
+                    if ($(this).val() === 'otra') {
+                        $('#otraMarcaDiv').show();
+                    } else {
+                        $('#otraMarcaDiv').hide();
+                    }
+                });
+            }
+        } else {
+            dynamicDiv.empty();
         }
     }
 
@@ -57,9 +94,17 @@ $(document).ready(function () {
             buttonPrev.css('display', 'none');
             buttonSend.css('display', 'none');
             case_use.css('display', 'block');
+        } else if (step === questions.length) {
+            buttonNext.css('display', 'none');
+            buttonSend.css('display', 'none');
+            buttonReview.css('display', 'inline');
+            case_use.css('display', 'none');
+            dynamicDiv.append();
         } else {
             buttonPrev.css('display', 'inline');
+            buttonNext.css('display', 'inline');
             buttonSend.css('display', 'none');
+            buttonReview.css('display', 'none');
             case_use.css('display', 'none');
         }
     }
@@ -68,8 +113,23 @@ $(document).ready(function () {
     function nextStep() {
         // Guardar el valor seleccionado en allvalues
         if (Step > 0) {
-            const question = questions[Step - 1];
-            allvalues[question.id.charAt(0).toUpperCase() + question.id.slice(1)] = $(`#${question.id}`).val();
+            const question = questions.find(q => q.position === Step);
+            if (question.type === 'checkbox') {
+                allvalues[question.id.charAt(0).toUpperCase() + question.id.slice(1)] = [];
+                question.options.forEach(option => {
+                    if ($(`#${option.value}`).is(':checked')) {
+                        allvalues[question.id.charAt(0).toUpperCase() + question.id.slice(1)].push(option.value);
+                        if (question.id === 'conexiones') {
+                            allvalues.conexiones.push(option.value);
+                        }
+                    }
+                });
+            } else {
+                allvalues[question.id.charAt(0).toUpperCase() + question.id.slice(1)] = $(`#${question.id}`).val();
+                if (question.id === 'marca' && $(`#${question.id}`).val() === 'otra') {
+                    allvalues.OtraMarca = $('#otraMarcaInput').val();
+                }
+            }
         } else {
             allvalues.Proposito = case_use.val();
         }
@@ -88,18 +148,53 @@ $(document).ready(function () {
     // Función que nos lleva a la pregunta anterior en el formulario
     function previusStep() {
         if (Step > 0) {
+            const question = questions.find(q => q.position === Step);
+            if (question.type === 'checkbox') {
+                allvalues[question.id.charAt(0).toUpperCase() + question.id.slice(1)] = [];
+            } else {
+                allvalues[question.id.charAt(0).toUpperCase() + question.id.slice(1)] = '';
+                if (question.id === 'marca') {
+                    allvalues.OtraMarca = '';
+                }
+            }
+
             Step -= 1;
             allStep.pop();
             dynamicDiv.empty();
             if (Step === 0) {
                 case_use.css('display', 'block');
                 firstDiv.css('display', 'block');
+                Divreview.css('display', 'none');
             } else {
                 generateQuestion(Step);
             }
             hidden(Step);
+            Divreview.css('display', 'none'); // Ocultar el resumen de respuestas
         }
         console.log(allvalues);
+    }
+
+    function review() {
+        allvalues.conexiones = allvalues.conexiones.join(', ');
+        allvalues.Extras = allvalues.Extras.join(', ');
+        Divreview.empty();
+        Divreview.append(`<h3>Resumen de tus respuestas</h3>
+                            <p>Propósito: ${allvalues.Proposito}</p>
+                            <p>Rendimiento: ${allvalues.Rendimiento}</p>
+                            <p>Portabilidad: ${allvalues.Portabilidad}</p>
+                            <p>Presupuesto: ${allvalues.Presupuesto}</p>
+                            <p>Almacenamiento: ${allvalues.Almacenamiento}</p>
+                            <p>Velocidad: ${allvalues.Velocidad}</p>
+                            <p>Batería: ${allvalues.Bateria}</p>
+                            <p>Conexiones: ${allvalues.conexiones}</p>
+                            <p>Cantidad de puertos: ${allvalues.CantPuertos}</p>
+                            <p>Extras: ${allvalues.Extras}</p>`);
+        let marca = allvalues.marca;
+        if (allvalues.marca === 'otra' && allvalues.OtraMarca.trim() !== '') {
+            marca = allvalues.OtraMarca;
+        }
+        Divreview.append(`<p>Marca: ${marca}</p>`);
+        firstDiv.css('display', 'none');
     }
 
     // Evento para actualizar `casoDeUso` y pasar al siguiente paso
@@ -110,6 +205,11 @@ $(document).ready(function () {
     // Evento para el botón "Anterior"
     buttonPrev.click(function () {
         previusStep();
+    });
+
+    // Evento para el botón "Revisar"
+    buttonReview.click(function () {
+        review();
     });
 
     // Inicialización
